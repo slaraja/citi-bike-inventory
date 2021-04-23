59aca93aa23c1d82cae875d3ecfe307ff90e40ae
Stephanie La Raja <stephanielaraja@gmail.com>
2021-04-23 08:00:58 -0400

    index view update

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,18 +1,18 @@
 class UserController < ApplicationController
 
-get '/signup' do
+get "/signup" do
     erb :"users/new"
 end
 
 post "/signup" do
     #handles signing up the user
-    binding.pry
+    #binding.pry
     #instantiate user
     u = User.new(params)
     #params is key value pairs. 
     #instead of params could also write: User.new(email: params["email"], password: params["password"])
     #make sure user signed up with valid data
-    if u.email.blank? || !u.password.blank? || User.find_by_email(params[":email"])   
+    if u.email.blank? || u.password.blank? || User.find_by_email(params[":email"])   
     #checks to see if all values are filled in and if the email is already used
     redirect to '/signup'
     else #a valid attempt
